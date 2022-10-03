c0459eba08265f66313c0209f175210c6e28bce8
Rey Mooy <gdrrey@gmail.com>
2022-10-03 21:35:03 +0800

    fix error

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,12 +2,10 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
 import { disabilitiesRouter } from "./disabilities";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
   .merge("disabilities.", disabilitiesRouter)
 
 // export type definition of API
